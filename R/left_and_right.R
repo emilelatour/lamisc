b1dafdc25ffc17ab6f8fc2b5bc4cf5a55404410f
Emile Latour <latour@ohsu.edu>
2018-06-26 15:43:38 -0700

    Fixed an error. Uses stringr now; more consistent

--- R/left_and_right.R
+++ R/left_and_right.R
@@ -1,5 +1,3 @@
-
-
 #' @title
 #' Return text to the left and the right of a string
 #'
@@ -13,21 +11,40 @@
 #' there's a bit of nostalgia for me in using these functions. But I also
 #' think that the naming makes it clearer what is going on.
 #'
-#' A couple of reference:
-#'   + \url{https://stackoverflow.com/questions/45070117/r-check-if-special-character-in-string}
-#'   + \url{https://stackoverflow.com/questions/36928870/r-check-if-string-contains-special-characters}
+#' @references
+#' \url{https://stackoverflow.com/questions/45070117/r-check-if-special-character-in-string}
+#'
+#' \url{https://stackoverflow.com/questions/36928870/r-check-if-string-contains-special-characters}
 #'
 #' @param string The string
 #' @param char A character used to delimit the string
+#'
+#' @importFrom stringr str_sub
+#' @importFrom stringr str_locate
 #'
 #' @return A string
 #' @export
 #'
 #' @examples
-#' a <- c("asdasd.sss","segssddfge.sss","se.sss")
+#' a <- c("left.right","left.right","left.right")
 #' left(a, ".")
 #' right(a, ".")
 #'
+#' library(dplyr)
+#' library(tibble)
+#'
+#' test_df <- tibble::tibble(
+#'   label = c("resection_margin=Mohs, > 1 cm",
+#'             "resection_margin=Mohs, > 1 cm",
+#'             "resection_margin=Mohs, <= 1 cm",
+#'             "  resection_margin=Mohs, > 1 cm"))
+#' test_df %>%
+#'   mutate(left = left(label, "="),
+#'          right = right(label, "="))
+#'
+#' right(test_df$label, "=")
+#' left(test_df$label, "=")
+#' stringr::str_trim(left(test_df$label, "="), side = "both")
 #'
 
 left <- function(string, char) {
@@ -40,10 +57,14 @@ left <- function(string, char) {
     esc <- ""
   }
 
-  substr(string,
-         start = 1,
-         stop = unlist(gregexpr(paste0(esc, char), string)) - 1
-  )
+  # substr(string,
+  #        start = 1,
+  #        stop = unlist(gregexpr(paste0(esc, char), string)) - 1)
+
+  stringr::str_sub(string,
+                   start = 1,
+                   end = stringr::str_locate(string,
+                                               paste0(esc, char))[, 1] - 1)
 
 }
 
@@ -60,10 +81,19 @@ right <- function(string, char) {
     esc <- ""
   }
 
-  substr(string,
-         start = unlist(gregexpr(paste0(esc, char), string)) + 1,
-         stop = nchar(string)
-  )
+  # substr(string,
+  #        start = unlist(gregexpr(paste0(esc, char), string)) + 1,
+  #        stop = nchar(string))
+
+  stringr::str_sub(string,
+                   start = stringr::str_locate(string,
+                                               paste0(esc, char))[, 1] + 1,
+                   end = nchar(string))
+
 
 }
+
+
+
+
 
