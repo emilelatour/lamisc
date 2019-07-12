d5e2fa2f711a3b47125875c8302438a40d339a42
Emile Latour <latour@ohsu.edu>
2019-07-12 10:12:25 -0700

    Change method used for calc_date_diff

--- R/calc_date_diff.R
+++ R/calc_date_diff.R
@@ -6,6 +6,14 @@
 #' months, weeks, days, hours, minutes, or seconds. Really just a convenience
 #' wrapper for lubridate functions
 #'
+#'  Divide an interval by a period to determine its implied length in clock
+#'  time. Periods track changes in clock times, which ignore time line
+#'  irregularities.
+#'
+#'  Divide and interval by a duration to determine its physical length.
+#'  Durations track the passage of physical time, which deviates from clock time
+#'  when irregularities occur.
+#'
 #' @references
 #'
 #' \url{http://data.library.virginia.edu/working-with-dates-and-time-in-r-using-the-lubridate-package/}
@@ -22,9 +30,18 @@
 #'   "seconds". Default is "days".
 #' @param precise TRUE (default) for fractional diff or FALSE for integer (not
 #'   rounded) diff.
+#' @param length Options: "physical" or "implied". Per the lubridate cheat
+#'   sheet, divide an interval by a __duration__ to determine its __physical__
+#'   length; divide by a __period__ to determine its __implied__ length in clock
+#'   time. The default is "implied" and this is what you would want to use most
+#'   of the time when calculating age, time to event, etc.
 #'
 #' @usage
-#' calc_date_diff(start, end = Sys.Date(), units = "days", precise = TRUE)
+#' calc_date_diff(start,
+#'                end = Sys.Date(),
+#'                units = "days",
+#'                precise = TRUE,
+#'                length = "implied")
 #'
 #' @return A numeric object or vector
 #' @export
@@ -36,8 +53,15 @@
 #' @importFrom lubridate ddays
 #' @importFrom lubridate dhours
 #' @importFrom lubridate dminutes
+#' @importFrom lubridate dseconds
 #' @importFrom lubridate as.duration
 #' @importFrom lubridate interval
+#' @importFrom lubridate years
+#' @importFrom lubridate weeks
+#' @importFrom lubridate days
+#' @importFrom lubridate hours
+#' @importFrom lubridate minutes
+#' @importFrom lubridate seconds
 #'
 #' @examples
 #' library(dplyr)
@@ -85,7 +109,8 @@
 calc_date_diff <- function(start,
                            end = Sys.Date(),
                            units = "days",
-                           precise = TRUE) {
+                           precise = TRUE,
+                           length = "implied") {
 
   if (any(!inherits(start,
                     c("POSIXt", "POSIXct", "POSIXlt", "Date", "character"))))
@@ -95,21 +120,37 @@ calc_date_diff <- function(start,
                     c("POSIXt", "POSIXct", "POSIXlt", "Date", "character"))))
     stop("date(s) not in POSIXt or Date or character format")
 
-  denom <- dplyr::case_when(
-    units == "years" ~ lubridate::dyears(1),
-    units == "months" ~ lubridate::dyears(1) / 12,
-    units == "weeks" ~ lubridate::dweeks(1),
-    units == "days" ~ lubridate::ddays(1),
-    units == "hours" ~ lubridate::dhours(1),
-    units == "minutes" ~ lubridate::dminutes(1),
-    units == "seconds" ~ 1
-  )
+  if (length == "implied") {
+    # divide an interval by a period to determine its implied length in clock
+    # time. Periods track changes in clock times, which ignore time line
+    # irregularities.
+    denom <- dplyr::case_when(
+      units == "years" ~ lubridate::years(1),
+      units == "months" ~ months(1),
+      units == "weeks" ~ lubridate::weeks(1),
+      units == "days" ~ lubridate::days(1),
+      units == "hours" ~ lubridate::hours(1),
+      units == "minutes" ~ lubridate::minutes(1),
+      units == "seconds" ~ lubridate::seconds(1))
+
+  } else {
+    # Divide and interval by a duration to determine its physical length.
+    # Durations track the passage of physical time, which deviates from clock
+    # time when irregularities occur.
+    denom <- dplyr::case_when(
+      units == "years" ~ lubridate::dyears(1),
+      units == "months" ~ lubridate::dyears(1) / 12,
+      units == "weeks" ~ lubridate::dweeks(1),
+      units == "days" ~ lubridate::ddays(1),
+      units == "hours" ~ lubridate::dhours(1),
+      units == "minutes" ~ lubridate::dminutes(1),
+      units == "seconds" ~ lubridate::dseconds(1))
+  }
 
   if (precise == TRUE) {
-    lubridate::as.duration(
-      lubridate::interval(start = start, end = end)) / denom
+    lubridate::interval(start = start, end = end) / denom
   } else {
-    lubridate::as.duration(
-      lubridate::interval(start = start, end = end)) %/% denom
+    lubridate::interval(start = start, end = end) %/% denom
   }
 }
+
